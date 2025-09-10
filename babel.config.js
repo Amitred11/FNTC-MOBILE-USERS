715674da2f27b099e077dcf3c9a4c83c510ba976
Leo Ama <amadoreleoncio@gmail.com>
2025-09-10 20:37:58 +0800

    fix: api plain

--- babel.config.js
+++ babel.config.js
@@ -7,3 +7,17 @@ module.exports = function(api) {
     ].filter(Boolean),
   };
 };
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: true,
+      },
+    ],
+  ],
+};
--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,14 +1,9 @@
-// config/apiConfig.js
+import { API_URL, INTERNAL_API_KEY, CHATBOT_API_ENDPOINT } from '@env';
 
-// For Android Emulator, the host machine is at 10.0.2.2
-// For iOS Simulator, the host machine is at 'localhost'
-// For a physical device on the same Wi-Fi, use your computer's local IP (e.g., '192.168.1.5')
-const API_URL = 'backend/api'; // Change this IP for your setup
-const Config_INTERNAL_API_KEY = 'no';
-const CHATBOT_API_ENDPOINT = 'backend';
 const apiConfig = {
   API_URL,
-  Config_INTERNAL_API_KEY,
+  Config_INTERNAL_API_KEY: INTERNAL_API_KEY,
   CHATBOT_API_ENDPOINT,
 };
+
 export default apiConfig;
